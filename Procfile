a6766c4e88ee179f27cdf5910896d182aebdc716
= <=>
2018-07-05 00:26:15 +0800

    尝试部署在heroku上

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn zhihuer.wsgi
--- zhihuer/settings.py
+++ zhihuer/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'q8oz7zb$zr^ptqwr2j(w6ivdvfu%dcrh8y8tje#*)h3hwci&px' #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #True
+DEBUG = True #True
 
 ALLOWED_HOSTS = ['*'] #[]
 
@@ -186,3 +186,7 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
     }
 }
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
